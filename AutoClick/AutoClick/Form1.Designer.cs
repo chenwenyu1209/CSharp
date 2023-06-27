ee891f211e355ff906783dcd61a1e4c8eb6c0c87
KissBite <1219796554@qq.com>
2023-06-28 05:31:51 +0800

    AutoClick
    A Leisure C# Software

---
+++ AutoClick/AutoClick/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace AutoClick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label13 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label11.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label11.Location = new System.Drawing.Point(349, 73);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(71, 16);
+            this.label11.TabIndex = 28;
+            this.label11.Text = "等待启动";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("黑体", 14F);
+            this.label10.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label10.Location = new System.Drawing.Point(218, 71);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(149, 19);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "当前软件状态：";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Window;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(12, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 27);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label9.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label9.Location = new System.Drawing.Point(42, 291);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(383, 16);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "此软件永久免费 如若您在淘宝等店铺购买请要求退款";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label7.Location = new System.Drawing.Point(22, 228);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(161, 14);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "3.延迟毫秒数请自行调整";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label6.Location = new System.Drawing.Point(22, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(350, 14);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "2.在按键里触发指令后 鼠标放到游戏的 [确定] 按键上";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label5.Location = new System.Drawing.Point(22, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(273, 14);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "1.在按键里设置触发时间和精调延迟毫秒数";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(333, 36);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(40, 21);
+            this.textBox4.TabIndex = 17;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label4.Location = new System.Drawing.Point(218, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 19);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "延迟毫秒数";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 91);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "请设定攻防排队时间";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(167, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "秒";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "分";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "时";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(135, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(30, 21);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(30, 21);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(30, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.groupBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.groupBox2.Location = new System.Drawing.Point(12, 154);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(450, 170);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "教程";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("黑体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label12.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.label12.Location = new System.Drawing.Point(10, 96);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(434, 14);
+            this.label12.TabIndex = 32;
+            this.label12.Text = "4.点击\"暂停\"后会变成\"继续\" 若要正常运行点击功能请再点击\"继续\"";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Window;
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(122, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 27);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "暂停";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.ForeColor = System.Drawing.Color.IndianRed;
+            this.label8.Location = new System.Drawing.Point(218, 105);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(231, 16);
+            this.label8.TabIndex = 32;
+            this.label8.Text = "此按键务必启动管理员身份运行";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.linkLabel1.Location = new System.Drawing.Point(268, 140);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(197, 12);
+            this.linkLabel1.TabIndex = 33;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "B站ID Emo电台男主播 点击这里查看";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label13.ForeColor = System.Drawing.Color.IndianRed;
+            this.label13.Location = new System.Drawing.Point(218, 137);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(47, 16);
+            this.label13.TabIndex = 34;
+            this.label13.Text = "教程:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(484, 336);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "攻防自动排队 C#V1.1 @ 亲咬 - 双梦镇";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ AutoClick/AutoClick/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AutoClick
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();//窗口初始化
+        }
+
+        //启动暂停
+        ManualResetEvent OnOff = new ManualResetEvent(true);
+
+        // 定义静态类NativeMethods 将DllImport特性应用于mouse_event方法声明上 注：DllImport特性只能用于方法声明上不可以在类中直接使用该特性。
+        public static class NativeMethods
+        {
+            [DllImport("user32.dll")]
+            public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+        }
+
+        //窗口初始化及自动获取系统当前时间功能
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //点击确定后 获取设定的小时、分钟、秒
+        public void button1_Click(object sender, EventArgs e)
+        {
+
+
+            int Tickle = 1;
+            if (Tickle == 1 && isPaused == false)
+            {
+                label11.Text = "功能启用";
+                label11.ForeColor = Color.Red;
+            }
+
+            //打断While
+            int Interrupt = 1;
+
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(100);
+
+                // 鼠标常数
+                const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+                const int MOUSEEVENTF_LEFTUP = 0x0004;
+                //将小时、分钟、秒设置为int格式
+                int Settime_hour =Convert.ToInt32(0+ textBox1.Text);
+                int Settime_minute =Convert.ToInt32(0 + textBox2.Text);
+                int Settime_second = Convert.ToInt32(0 + textBox3.Text);
+                int Settime_delay = Convert.ToInt32(0 + textBox4.Text);
+
+                Console.WriteLine("设定小时：" + Settime_hour);
+                Console.WriteLine("设定分钟：" + Settime_minute);
+                Console.WriteLine("设定秒数：" + Settime_second);
+                Console.WriteLine("设定秒数：" + Settime_delay);
+                while (Interrupt == 1 && !isPaused)
+                {
+                    TimeZoneInfo chinaTimeZone = TimeZoneInfo.FindSystemTimeZoneById("China Standard Time");
+                    DateTime currentChinaTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, chinaTimeZone);
+                    Console.WriteLine(currentChinaTime);
+                    int currentHour = currentChinaTime.Hour; // 获取当前小时数
+                    int currentMinute = currentChinaTime.Minute; // 获取当前分钟数
+                    int currentSecond = currentChinaTime.Second; // 获取当前秒钟数
+
+
+                    //开始判断
+                    if (Settime_hour == currentHour && Settime_minute == currentMinute && Settime_second <= currentSecond)
+                    {
+                        //添加延迟毫秒数
+                        Thread.Sleep(Settime_delay);
+                        Console.WriteLine("开始点击");
+                        for (int i = 0; i < 5; i++)
+                        {
+                            // 使用 NativeMethods 类的 mouse_event 方法触发鼠标点击
+                            // 模拟鼠标左键按下
+                            NativeMethods.mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                            Thread.Sleep(10); // 延时一段时间，确保鼠标点击生效
+                            // 模拟鼠标左键释放
+                            NativeMethods.mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                            Interrupt = 0;
+                        }
+                    }
+                }
+            });
+        }
+
+         
+
+        // 暂停键
+        public bool isPaused = false;
+        public void button2_Click(object sender, EventArgs e)
+        {
+            if (isPaused)
+            {
+                isPaused = false;
+                button2.Text = "暂停";
+                label11.Text = "请再次点击确定";
+                label11.ForeColor = Color.Blue;
+            }
+            else
+            {
+                isPaused = true;
+                button2.Text = "继续";
+                label11.Text = "功能停止";
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.jx3box.com/tool/63302");
+        }
+    }
+}
